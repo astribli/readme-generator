7b5ce1eccd0147c67f291df239fb0c585e2d9de0
astribli <andrewjstribling@gmail.com>
2020-10-04 23:01:02 -0500

    generate markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `## ${data.title}
+![GitHub license](https://img.shields.io/badge/license-${data.license}-green)
+## Description:
+${data.description}
+## Table of Contents:
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+## Installation:
+${data.installation}
+## Usage:
+${data.usage}
+## License:
+Licensed by ${data.license}
+## Contributing:
+${data.guidelines}
+## Tests:
+${data.test}
+## Questions:
+If you have any questions, feel free to look up ${data.github} at https://github.com/${data.github}
+or email at ${data.email}`;
 }
 
 module.exports = generateMarkdown;
